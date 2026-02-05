2ab9f940c459da96554f101dac671ea495c486b6
Carlos <nicolaugalves@gmail.com>
2026-02-04 21:58:55 -0300

    trying to upload in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/make-life-better-react/' : '/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        store: resolve(__dirname, 'store.html'),
+      },
+    },
+  },
+}))
