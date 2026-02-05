e267c39b272b521355282275028e59daaf7d4a9f
Carlos <nicolaugalves@gmail.com>
2026-02-04 22:16:00 -0300

    testing warns

--- src/legacy/config/firebase.config.js
+++ src/legacy/config/firebase.config.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDcHBBJC1BHAIDO8vSGQsNSN3y_e6sTmBM",
-    authDomain: "makelifebetter-7f3c9.firebaseapp.com",
-    projectId: "makelifebetter-7f3c9",
-    storageBucket: "makelifebetter-7f3c9.firebasestorage.app",
-    messagingSenderId: "749904577078",
-    appId: "1:749904577078:web:3235cbb8e26ffaabba0e97",
-    measurementId: "G-ZPNC0MZ1T7"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
